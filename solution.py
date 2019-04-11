be212b795dd0dd92cfcfd6cff227da1630d2e2c8
nip403 <41174986+nip403@users.noreply.github.com>
2019-04-11 13:00:23 +0100

    Update solution.py

--- solution.py
+++ solution.py
@@ -48,7 +48,7 @@ while True:
         continue # restarts at the top of the loop
 
     # to convert the day into an index in a list (e.g. Mon1 -> 0, Tue2 -> 6) - see README for more info
-    index = daynames.index(day[:3])+(int(day[-1])-1)*5
+    index = daynames.index(day[:3]) + (int(day[-1])-1)*5
 
     total = 0 # total amount of late buses
     for busindex,bus in enumerate(buses): # keeps track of index and item each loop
